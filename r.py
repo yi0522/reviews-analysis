29323b72c138b3f336b46c23a295b94c7ae2c5cb
yi0522 <99787135+yi0522@users.noreply.github.com>
2022-05-02 15:56:35 +0800

    Add files via upload

---
+++ r.py
@@ -0,0 +1,28 @@
+data =  []
+count = 0
+with open('original.txt','r') as f:
+	for line in f:
+		data.append(line)
+		count = count + 1
+		if count % 1000 == 0:
+			print(len(data))
+print('檔案讀取完了','總共有',len(data),'筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('平均留言長度',sum_len/len(data))
+
+new = []
+for d in data:
+	new.append(d)
+	print('一共有',len(new),'留言長度小於100')
+    print(new[0])
+
+  good = []
+  for d in data:
+  	if 'good' in d:
+  		    good.append(d)
+print('一共有',len(good),'留言提到good')
+print(good[0])
+	
